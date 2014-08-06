cddf1ca6acc57295359df65dad1ffc56850a77a1
Michal Jach <michal@jach.me>
2014-08-06 19:02:56 +0200

    admin RWD and post selection

--- templates/default/admin_index.php
+++ templates/default/admin_index.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0">
         <title>section2 - Administration</title>
         <link rel="stylesheet" href="templates/<?=TEMPLATE?>/css/style_admin.css">
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
